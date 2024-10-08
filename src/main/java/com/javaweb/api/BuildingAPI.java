267a26faa76643538d2e45ff7efb813995d7378f
3msinhvien <dotung.ptit@gmail.com>
2024-10-08 23:14:00 +0700

    su dung map de nhan du lieu

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,5 +1,6 @@
 package com.javaweb.api;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,9 @@ public class BuildingAPI {
 	private BuildingService buildingService;
 	
 	@GetMapping (value="/api/building/")
-	public List<BuildingDTO> getBuilding (@RequestParam(name = "name", required = false) String name,
-										@RequestParam (name = "districtid", required = true) Long district,
-										@RequestParam (name = "typeCode", required = false) List<String> typeCode) {
-		List<BuildingDTO> result = buildingService.findAll(name , district);
+	public List<BuildingDTO> getBuilding (@RequestParam Map<String, Object> params )
+	{
+		List<BuildingDTO> result = buildingService.findAll(params);
 		return result;
 	}
 	
--- src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
+++ src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
@@ -7,27 +7,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.javaweb.repository.BuildingRepository;
 import com.javaweb.repository.entity.BuildingEntity;
 
-@Repository
-	public class BuildingRepositoryImpl implements BuildingRepository{
+	@Repository
+public class BuildingRepositoryImpl implements BuildingRepository{
 	static final String DB_URL = "jdbc:mysql://localhost:3306/estatebasic" ; 
 	static final String USER = "root";
 	static final String PASS = "tung.2802";
 
 	@Override
-	public List<BuildingEntity> findAll(String name, Long districtId) {
+	public List<BuildingEntity> findAll(Map<String, Object> params) {
 		StringBuilder sql = new StringBuilder("SELECT * FROM building b WHERE 1 = 1 ");
-		if( name != null && !name.equals("") ) {
-			sql.append("AND b.name like '%" + name + "%' ");
-		}
-		if (districtId != null) {
-			sql.append("AND b.districtId = "+ districtId + " ");
-		}
+		
 		List<BuildingEntity> res = new ArrayList<>();
 		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 					Statement stmt = conn.createStatement();
